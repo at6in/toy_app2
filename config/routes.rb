a919d041fd3b2605920d9bcc4f456f047fda4bda
Atbin Jahandideh <atbinjahandideh@gmail.com>
2017-11-10 17:50:15 +0000

    Finish toy-app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  root 'resources#index'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
